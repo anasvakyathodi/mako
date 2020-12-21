be5227712d09b1436d51d5b2902218efb72c92ff
anasvakyathodi <anasvakyathodi@gmail.com>
2020-12-21 15:02:32 +0530

    footer change

---
+++ src/Components/Register/Components/Footer/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import FooterBG from "./footer.svg";
+const Footer = () => {
+  return (
+    <div>
+      <img src={FooterBG} width="100%" alt="footer-bg" />
+    </div>
+  );
+};
+
+export default Footer;
--- src/Components/Register/index.js
+++ src/Components/Register/index.js
@@ -3,7 +3,7 @@ import Banner from "./Components/Banner";
 import Section3 from "./Components/Section3";
 import Section4 from "./Components/Section4";
 import ClosingStatement from "./Components/ClosingStatement";
-import Footer from "../LandingPage/Layout/Footer";
+import Footer from "./Components/Footer";
 const Register = () => {
   return (
     <>
